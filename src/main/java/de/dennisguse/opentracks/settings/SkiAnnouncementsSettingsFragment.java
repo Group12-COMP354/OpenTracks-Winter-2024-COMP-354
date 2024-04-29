18c79383e2e15993e4c55a0b17b926ebb7fe3768
Alexanne Marcil <alexannemarcil@gmail.com>
2024-04-28 23:35:44 -0400

    Added a record to allow retrieval of the temperature at a certain location

--- src/main/java/de/dennisguse/opentracks/settings/SkiAnnouncementsSettingsFragment.java
+++ src/main/java/de/dennisguse/opentracks/settings/SkiAnnouncementsSettingsFragment.java
@@ -17,6 +17,6 @@ public class SkiAnnouncementsSettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onStart() {
         super.onStart();
-        ((SettingsActivity) getActivity()).getSupportActionBar().setTitle(R.string.settings_announcements_statistics_title);
+        ((SettingsActivity) getActivity()).getSupportActionBar().setTitle(R.string.settings_ski_announcements_statistics_title);
     }
 }
